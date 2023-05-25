b9be5c1a40d99a15c702623a7696bb1ab1f47bc9
abhinav <abhinavlv@yahoo.com>
2023-05-25 21:08:18 +0530

    made changes to cookie settings

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ const app = express()
 
 app.use(cors())
 
+app.use(function(req, res, next) {  
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+}); 
+
 // Middleware
 app.use(express.json())
 app.use(session({
