13ba315d17808e2ec7ec76176849367de3119cb2
abhinav <abhinavlv@yahoo.com>
2023-05-30 20:30:39 +0530

    trying

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: {secure: true, maxAge: 2*60*60*1000, httpOnly: true, sameSite: 'none'} // 2 hours
+    cookie: {secure: false, maxAge: 2*60*60*1000, httpOnly: true, sameSite: 'none'} // 2 hours
 }));
 
 app.use(cors())
