133909b47ea54671baeb7832267fbccf8d2ba6fc
abhinav <abhinavlv@yahoo.com>
2023-05-31 09:15:18 +0530

    wedtry3

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: {secure: true, maxAge: 2*60*60*1000, httpOnly: true, path:'/', sameSite: 'none',} // 2 hours
+    cookie: {secure: false, maxAge: 2*60*60*1000, httpOnly: false, sameSite: 'none'} // 2 hours
 }));
 
 // Root route
